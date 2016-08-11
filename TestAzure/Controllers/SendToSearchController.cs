da717bcc3e41c9e5f90335f0200999e9bbf44663
helen12 <helenwond25@gmail.com>
2016-08-11 10:53:41 -0700

    myfirstcommit

---
+++ TestAzure/Controllers/SendToSearchController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestAzure.Models;
+
+namespace TestAzure.Controllers
+{
+    public class SendToSearchController : Controller
+    {
+        // GET: SendToSearch
+        public ActionResult Index()
+        {
+            //SearchData searchedRes = new SearchData();
+            //searchedRes.searchSentence = Request.Form["sent"];
+            //searchedRes.searchKey = Request.Form["srk"];
+            ////String val1, val2;
+            //WebClient proxy = new WebClient();
+
+            //string serviceURL = string.Format("http://localhost:2009/WordSearchService.svc/Search/searchSentence/searchKey");
+            ////Console.WriteLine("HI");
+            ////Console.ReadKey();
+            //byte[] data = proxy.DownloadData(serviceURL);
+            //Stream stream = new MemoryStream(data);
+            //var reader = new StreamReader(stream);
+            //var result = reader.ReadToEnd();
+            
+
+            ////SServiceReference1.WordSearchServiceClient clt1 = new SServiceReference1.WordSearchServiceClient();
+            ////string result=clt1.Search("abebe beso albelam eko", "eko");
+            
+            //searchedRes.searchSentence = result;
+            //searchedRes.searchKey = "Eko";
+
+            return View();
+        }
+
+
+        public ActionResult SearchWord()
+        {
+            SearchData searchedRes = new SearchData();
+            searchedRes.searchSentence = Request.Form["sent"];
+            searchedRes.searchKey = Request.Form["srk"];
+            //String val1, val2;
+            WebClient proxy = new WebClient();
+
+            string serviceURL = string.Format("http://localhost:2009/WordSearchService.svc/Search/searchSentence/searchKey");
+            //Console.WriteLine("HI");
+            //Console.ReadKey();
+            byte[] data = proxy.DownloadData(serviceURL);
+            Stream stream = new MemoryStream(data);
+            var reader = new StreamReader(stream);
+            var result = reader.ReadToEnd();
+
+
+            //SServiceReference1.WordSearchServiceClient clt1 = new SServiceReference1.WordSearchServiceClient();
+            //string result=clt1.Search("abebe beso albelam eko", "eko");
+
+            //searchedRes.searchSentence = result;
+            //searchedRes.searchKey = "Eko";
+            searchedRes.searchResult = result;
+
+            return View(searchedRes);
+        }
+    }
+}
---
+++ TestAzure/Models/SearchData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestAzure.Models
+{
+    public class SearchData
+    {
+        //test
+        public string searchSentence {set;get;}
+        public string searchKey { set; get; }
+        public string searchResult { set; get; }
+    }
+}
